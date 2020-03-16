57b7579659c4d2a9c60b6d20f57b28bb5e75e314
Matheus Nadaleti Garcia <matheus.garcia@opus-software.com.br>
2020-03-16 16:25:04 -0300

    Re-structuring folder structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import BurgerBuilder from './components/containers/BurgerBuilder/BurgerBuilder';
-import Layout from './components/Layout/Layout';
+import Layout from './components/hoc/Layout/Layout';
 
 function App() {
   return (
