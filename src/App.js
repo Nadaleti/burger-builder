2832091e71f228eb48cd74d2c99919c3f28c7bfe
Nadaleti <mat.nadaleti@hotmail.com>
2020-05-19 23:23:33 -0300

    Creating checkout component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 import BurgerBuilder from './components/containers/BurgerBuilder/BurgerBuilder';
 import Layout from './components/hoc/Layout/Layout';
+import Checkout from './components/containers/Checkout/Checkout';
 
 function App() {
   return (
     <div>
       <Layout>
         <BurgerBuilder />
+        <Checkout />
       </Layout>
     </div>
   );
